c479abd94fe698850a27f223eda1881bf0f1a1f7
Ace <kronicrespawn@gmail.com>
2022-07-27 20:32:04 -0500

    gameboard layout added

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const gameboard = (() => {
-    const array1 = [0, 1, 2, 3, 4 , 5, 6, 7, 8]
+    const boardStatus = [
+        'x', 'x', 'x',
+        'x', 'x', 'x',
+        'x', 'x', 'x'
+    ];
+
     const position = () => {
+
+    }
+    const player = () => {
         
     }
-    return array1
+    
+    const marker = () => {
+
+    }
+
+    return boardStatus
 })();
 
 
