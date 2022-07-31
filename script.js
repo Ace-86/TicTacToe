4c629471ee07a1837c78535091ebab60ed6d6671
Ace <kronicrespawn@gmail.com>
2022-07-31 11:24:03 -0500

    win conditions added

--- script.js
+++ script.js
@@ -34,6 +34,17 @@ const gameboard = (() => {
         '', '', 'X'
     ];
 
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
     round = 0;
     // populate board with boardArray
     const populateBoard = () => {
